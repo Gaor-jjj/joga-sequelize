1df2e4b348064d9ba88e7ce45afa00d5155327d4
gaor-jjj <kaurtamm1995@gmail.com>
2024-09-24 14:04:58 +0300

    Created author table and added table data

--- seeders/20240919094356-demo-articles.js
+++ seeders/20240919094356-demo-articles.js
@@ -31,15 +31,10 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       }])
-    ])
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Articles', null, {});
   }
 };
