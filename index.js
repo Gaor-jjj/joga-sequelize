112730dc70dd9e18c9e91f636f2c43516a0b3e87
gaor-jjj <kaurtamm1995@gmail.com>
2024-10-04 12:01:38 +0300

    Admin route and controller

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const authorRouter = require('./routes/author');
 app.use('/', articleRouter);
 app.use('/article', articleRouter);
 app.use('/author', authorRouter);
+app.use('admin/article', articleRouter)
 
 // listen requests
 app.listen(3000 , () => {
